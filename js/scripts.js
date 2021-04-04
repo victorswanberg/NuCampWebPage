9b6c4085d5733d5ec19d5e6024ff12c6b76e37dc
Victor Swanberg <victor.swanberg@sublimemedia.com>
2021-04-04 08:54:55 -0700

    adding up changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,23 @@
 AOS.init();
 //Toggle button on carasouel component
 
-
 $(function () {
-  $(".carousel").carousel({ interval: 2000 });
-  $("#carouselButton").click(function () {
-    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-      $(".carousel").carousel("pause");
-      $("#carouselButton").children("i").removeClass("fa-pause");
-      $("#carouselButton").children("i").addClass("fa-play");
-    } else {
-      $(".carousel").carousel("cycle");
-      $("#carouselButton").children("i").removeClass("fa-play");
-      $("#carouselButton").children("i").addClass("fa-pause");
-    }
-  });
-});
+  $(".carousel").carousel({ interval: 2000})
+  $(".carousel").on( "click", function() {
+      if($("#carouselButton").children("i").hasClass("fa-pause")){
+          $(".carousel").carousel("pause");
+          $("#carouselButton").children("i").removeClass("fa-pause");
+          $("#carouselButton").children("i").addClass("fa-play");
+      }else{
+          $(".carousel").carousel("cycle");
+          $("#carouselButton").children("i").removeClass("fa-play");
+          $("#carouselButton").children("i").addClass("fa-pause");
+      }
+  })
+  $("#loginButton").on( "click", function() {
+      $("#loginModal").modal("toggle");
+  })
+  $("#reserveButton").on( "click", function() {
+      $("#reserveModal").modal("toggle");
+  })
+})
